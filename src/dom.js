11144e4eec142edbc32aafbe7f3e35ef5394357f
s7lkr <s.7.a.l.k.3.r.8.9@gmail.com>
2025-08-25 00:11:56 +0300

    create and implement new button 'stop game'

--- src/dom.js
+++ src/dom.js
@@ -3,6 +3,7 @@ export const rulesTextElement = document.getElementById('rules-text');
 export const rulesButtonElement = document.getElementById('rules-btn');
 export const pHiddenElement = document.querySelector('p.hidden');
 
+
 // ---------- Intro/Menu Section -----------
 
 // Main menu container
@@ -14,14 +15,18 @@ export const gameModeElements = document.querySelectorAll('input[name=game-mode]
 // Player Select
 export const playerChoicesElement = document.querySelectorAll('input[name=player-choice]');
 
+// Selected options
+export const showModeElement = document.getElementById('selected-mode');
+export const showPlayerElement = document.getElementById('selected-player');
+
 // Start Game
 export const startButtonElement = document.getElementById('start-btn-container');
+
 
 // ----------- Game Section -----------
 
 // Main elements
 export const gameElement = document.querySelector('div.tic-tac-toe');
-export const showGameModeElement = document.querySelector('#game-mode-section p');
 export const roundElement = document.querySelector('div.round-status h3');
 export const cellElements = document.querySelectorAll('div.cell-container div');
 
@@ -31,7 +36,8 @@ export const messageElement = document.querySelector('span#round-message');
 
 // Buttons: Next, Restart, Main Menu
 export const nextRoundButtonElement = document.querySelector('.buttons button#next');
-export const restartButtonElement = document.querySelector('.buttons button#restart-btn');
+export const stopGameButtonElement = document.getElementById('stop-btn');
+export const resetButtonElement = document.querySelector('.buttons button#restart-btn');
 export const mainMenuButtonElement = document.getElementById('main-menu-btn');
 
 // Score table
