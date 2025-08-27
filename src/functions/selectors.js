dc051c0d618d7d5034cd7deb6e1496b2b1c5af31
s7lkr <s.7.a.l.k.3.r.8.9@gmail.com>
2025-08-27 23:00:55 +0300

    update state from selectors

--- src/functions/selectors.js
+++ src/functions/selectors.js
@@ -1,26 +1,35 @@
 import { showModeElement, showPlayerElement, pHiddenElement, cellElements, playerMarkElement } from '../dom';
+
 import { setState } from '../state';
+import { playerMove } from './moves';
+
 
 // GAME MODE
 export function gameModeSelector(event) {
     // 'Player vs Player', 'Player vs Computer' or 'Computer vs Computer'
     showModeElement.textContent = vaR.modes[event.target.id];
-    pHiddenElement.textContent = event.target.id;
+    // pHiddenElement.textContent = event.target.id;
+    const mode = event.target.value
     setState({
-        gameMode: pHiddenElement.textContent,
+        gameMode: mode,
         gameModeSelected: true,
     });
     
-    if (vaR.gameMode == 'cvc') {
+    if (mode == 'cvc') {
         cellElements.forEach(cell => cell.removeEventListener('click', playerMove));
     }
 }
 
 // Player Choice
 export function playerSelector(event) {
-    vaR.playerSelected = true;
+    // vaR.playerSelected = true;
+    setState({
+        playerSelected: true,
+        initialPlayerMark: event.target.value,
+        currentPlayerMark: event.target.value,
+    });
     playerMarkElement.textContent = event.target.value;     // X or O
-    initialPlayerMark = playerMarkElement.textContent;
+    // initialPlayerMark = playerMarkElement.textContent;
     showPlayerElement.textContent = 'Player:  ' + initialPlayerMark;
-    currentPlayerMark = playerMarkElement.textContent;
+    // currentPlayerMark = playerMarkElement.textContent;
 }
