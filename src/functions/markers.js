c6b7334740bbd227184eaf8db5145bc513910889
s7lkr <s.7.a.l.k.3.r.8.9@gmail.com>
2025-08-28 00:46:05 +0300

    add: inner cell painter function

--- src/functions/markers.js
+++ src/functions/markers.js
@@ -1,10 +1,18 @@
 import { cellElements } from "../dom";
 
+// private function
+function painter(cell, color) {
+    cellElements.item(cell).style.backgroundColor = color;
+}
+
 export function markWinCells(winCells, color='orange') {
     const [c1, c2, c3] = winCells;
-    cellElements.item(c1).style.backgroundColor = color;
-    cellElements.item(c2).style.backgroundColor = color;
-    cellElements.item(c3).style.backgroundColor = color;
+    // cellElements.item(c1).style.backgroundColor = color;
+    // cellElements.item(c2).style.backgroundColor = color;
+    // cellElements.item(c3).style.backgroundColor = color;
+    painter(c1, color);
+    painter(c2, color);
+    painter(c3, color);
 }
 
 export function unMarkWinCells(color='antiquewhite') {
